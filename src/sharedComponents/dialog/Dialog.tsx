b489513bd8a3db2f8a6fb0c276d6313d0ba4d4b0
Jason McCaffrey <98906276+jasonmccaffrey1973@users.noreply.github.com>
2026-09-18 11:38:04 -0400

    cleaned up styles and types in Media Manager still WIP

---
+++ src/sharedComponents/dialog/Dialog.tsx
@@ -0,0 +1,30 @@
+import type { dialogComponentProps, dialogProps } from "./dialog.types";
+import { StyledDialog } from "./dialog.styles";
+import SVGIcon from "../SVG/SVGIcon";
+import Button from "../Button/Button";
+
+const Dialog = ({title, children, footerButtons,  controls, dialogRef }: dialogComponentProps)=> {
+
+  const { closeDialog } = controls;
+
+  return (
+    <StyledDialog ref={dialogRef} as="dialog" {...({} as dialogProps)}>
+    <div className="dialog-wrapper">
+      <div className="dialog-header">
+        <h2>{title}</h2>
+        <button onClick={closeDialog}><SVGIcon icon="close"  /></button>
+      </div>
+      <div className="dialog-body">
+        {children}
+      </div>
+      <div className="dialog-footer">
+        {footerButtons?.map((button, index) => (
+          <Button key={index} onClick={button.onClick} color={button.color}>{button.label}</Button>
+        ))}
+      </div>
+    </div>
+    </StyledDialog>
+  );
+}
+
+export default Dialog;
